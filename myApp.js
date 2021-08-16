ab283bb527ad54130c14cc6f56739ff88a114de4
João Adherval <joaoadherval@gmail.com>
2021-08-16 20:52:15 -0300

    serving a html file through app.get() on myApp.js

--- myApp.js
+++ myApp.js
@@ -3,11 +3,16 @@ var app = express();
 
 console.log("Hello World")
 
+// Express evaluates routes from top to bottom. Commenting this method so the next version below is used instead
+//app.get("/", function(req, res) {
+//    res.send('Hello Express');
+//});
+
+absolutePath = __dirname + "/views/index.html";
+
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
 });
-
-
 
 
 
