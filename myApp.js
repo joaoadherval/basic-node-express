a080e08ecc13219f74e6680015d6e6dbe224bfd0
Joao Adherval <joaoadherval@gmail.com>
2021-08-18 19:56:53 -0300

    getting query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -54,7 +54,13 @@ app.get("/:word/echo", function(req, res) {
   });
 });
 
-
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 
 
 
