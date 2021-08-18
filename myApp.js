815cfa90e15a6e6fd645979ef4528a81059fa3cd
Joao Adherval <joaoadherval@gmail.com>
2021-08-18 20:46:51 -0300

    getting data from post requests

--- myApp.js
+++ myApp.js
@@ -66,6 +66,11 @@ app.get("/name", function(req, res) {
   });
 });
 
+app.post("/name", function(req, res) {
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
+
 
 
 
